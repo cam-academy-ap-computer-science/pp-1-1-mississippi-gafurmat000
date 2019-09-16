e420c06260bbdd2420a140a590a8cef0e45b8f2a
GAFUR <GAFUR@192.168.0.29>
2019-09-16 00:00:38 -0700

    Homework test mississippi.

---
+++ pp-1-1-mississippi-gafurmat000-master/mississippi11.java
@@ -0,0 +1,16 @@
+
+
+public class mississippi11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("M     M    IIIII     SSSSS      SSSSS     IIIII     SSSSS      SSSSS     IIIII    PPPPPP     PPPPPP     IIIII");
+		System.out.println("MM   MM      I      S     S    S     S      I      S     S    S     S      I      P     P    P     P      I");
+		System.out.println("M M M M      I      S          S            I      S          S            I      P     P    P     P      I");
+		System.out.println("M  M  M      I       SSSSS      SSSSS       I       SSSSS      SSSSS       I      PPPPPP     PPPPPP       I");
+		System.out.println("M     M      I            S          S      I            S          S      I      P          P            I");
+		System.out.println("M     M      I      S     S    S     S      I      S     S    S     S      I      P          P            I");
+		System.out.println("M     M    IIIII     SSSSS      SSSSS     IIIII     SSSSS      SSSSS     IIIII    P          P          IIIII");
+	}
+
+}
